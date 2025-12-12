390a8fecc086c28761c72397e1f7edabf6c06e5a
bealunala <luna.be@northeastern.edu>
2025-12-12 15:21:26 -0500

    fix assignments schema

---
+++ Kambaz/Assignments/schema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const assignmentsSchema = new mongoose.Schema(
+  {
+    _id: String,
+    title: String,
+    course: String,
+    description: Number,
+    availableDate: String,
+    dueDate: String,
+    availableUntilDate: String,
+    points: Number,
+  },
+  { collection: "assignments" }
+);
+export default assignmentsSchema;
